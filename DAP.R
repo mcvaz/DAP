c0e128179568c89631859c9a9a847a40b54488ad
Marcel Vaz <mcvaz@ucla.edu>
2016-11-23 10:47:51 -0800

    update

--- DAP.R
+++ DAP.R
@@ -11,9 +11,9 @@ logit = function(x) log(x/(1-x)) # creates logit function
 ilogit = function(x) exp(x)/(1+exp(x)) # creates inverse logit function
 
 # Data
-nn = matrix(c(71,174,52,53,107,142,49,238,86,103,49,241,357,348,516,512,186,218,36,41,70,80), nrow=11, ncol=2, byrow=TRUE) # number of people in each treatment group in each study
-yy = matrix(c(4,1,3,2,12,3,15,19,18,5,11,10,4,9,25,11,20,15,17,25,4,5), nrow=11, ncol=2, byrow=TRUE) # idem, but for the number of deaths after one year of the treatment
-yy/nn
+nn = matrix(c(71,174,52,53,107,142,49,238,86,103,49,241,357,348,516,512,186,218,36,41,70,80), nrow=11, ncol=2, byrow=T) # number of people in each treatment group in each study
+yy = matrix(c(4,1,3,2,12,3,15,19,18,5,11,10,4,9,25,11,20,15,17,25,4,5), nrow=11, ncol=2, byrow=F) # idem, but for the number of deaths after one year of the treatment
+round(yy/nn,2) # mortality rates
 
 # Defining priors
 
@@ -38,6 +38,26 @@ cat("
   ", fill = TRUE)
 sink()
 
+# Jags input info
+data = list(n=nn, y=yy, N=length(nn))
+inits = rep(
+  list(list(
+    pie = matrix(rep(0.5,22), ncol= 2, nrow=11, byrow=TRUE),
+    mu = rep(0,11), delta = rep(0,11),
+    mu0 = 0, tau.mu = 0, delta0 = 0, tau.delta = 0))
+  ,3)
+params = c("pie[1:11,1:2]","mu[1:11]","delta[1:11]","mu0","delta0","tau.mu","tau.delta")
 
 
 
+
+
+
+model.out = jags(data=data, inits= NULL, parameter=params, "model.txt", 
+                 n.chains=3, n.iter=101000, n.burnin=0, n.thin=20, DIC=F)
+
+#Treat the first 1000 iterations as a burn in	
+Output = AddBurnin(model.out$BUGSoutput$sims.array, burnin=1000,n.thin=1)
+
+# Table:
+summary <- Output$Burnin.Summary[25:46, -5]
